92582ec81ba1ba3cfb75967ca6482b132b0da990
SIT_YASUDA <SIT_YASUDA@ZZ25.starzen.local>
2017-11-06 10:37:26 +0900

    リポジトリ作成テスト-javaだけ

---
+++ src/JavaSource/Otameshi.java
@@ -0,0 +1,26 @@
+/**
+ *
+ */
+package JavaSource;
+
+/**
+ * @author SIT_YASUDA
+ *
+ */
+public class Otameshi {
+
+
+	/**コンストラクター*/
+	public Otameshi() {
+		super();
+	}
+
+	public int add(int a, int b) {
+		int ret;
+
+		ret = a + b + 1;
+
+		return ret;
+	}
+
+}
