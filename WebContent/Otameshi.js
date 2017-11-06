fda01b226f41fa46657bafef422297ce1edceefb
SIT_YASUDA <SIT_YASUDA@ZZ25.starzen.local>
2017-11-06 14:03:45 +0900

    テストコミット-Otameshi.js

--- WebContent/Otameshi.js
+++ WebContent/Otameshi.js
@@ -11,7 +11,7 @@ btn.onclick = function(){
 	}
 	else {
 		inp.style.backgroundColor = "#ffffff";
-		alert("「"+input.value + "」と入力。");
+		alert("「"+input.value + "」と入力されました。");
 	}
 
 var btn2 = document.getElementById("btn2");
